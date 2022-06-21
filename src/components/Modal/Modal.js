f47470cacd2672a40fbca05c41632b25c092eaf1
Sean Gordon <nodrog.naes@gmail.com>
2022-06-20 23:54:59 -0400

    reload button styling

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,6 +2,10 @@ import React from "react";
 import './modal.css';
 
 const Modal = ({ isCorrect, turn, solution }) => {
+    const reload = () => {
+        setTimeout(() => window.location.reload(), 300);
+    }
+
     return (
         <div className="modal">
             {isCorrect && (
@@ -9,7 +13,7 @@ const Modal = ({ isCorrect, turn, solution }) => {
                     <h1>You win!!</h1>
                     <p className="solution">The word was "{solution.toUpperCase()}"</p>
                     <p>You found the secret word in {turn > 1 ? `${turn} guesses` : `${turn} guess`}. Well done!</p>
-                    <button id='submit' onClick={() => window.location.reload()}>Play again</button>
+                    <button id='submit' onClick={reload}>Play again</button>
                 </div>
             )}
             {!isCorrect && (
@@ -17,7 +21,7 @@ const Modal = ({ isCorrect, turn, solution }) => {
                     <h1>You lost!</h1>
                     <p className="solution">The word was "{solution.toUpperCase()}"</p>
                     <p>Better luck next time!</p>
-                    <button onClick={() => window.location.reload()} onKeyDown={() => window.location.reload()}>Try again</button>
+                    <button onClick={reload}>Try again</button>
                 </div>
             )}
         </div>
